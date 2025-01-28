9ea360e5d90800e8e2380093c86bed129f939825
ryukftw <workforbijoy9@gmail.com>
2025-01-28 13:00:17 +0000

    sm8250-common: Patch camera blobs to use libstdc++_vendor
    Fixes:
    error: vendor/realme/spartan/Android.bp:20943:1: dependency "libstdc++" of "libYTCommon" missing variant:
      os:android,image:vendor,link:shared
    available variants:
      os:linux_glibc,arch:x86_64,link:static
      os:linux_glibc,arch:x86_64,link:shared
      os:linux_glibc,arch:x86,link:static
      os:linux_glibc,arch:x86,link:shared
      os:windows,arch:x86,link:static
      os:windows,arch:x86,link:shared
      os:windows,arch:x86_64,link:static
      os:windows,arch:x86_64,link:shared
      os:android,arch:arm64_armv8-a,link:static
      os:android,arch:arm64_armv8-a,link:shared
      os:android,arch:arm_armv8-a,link:static
      os:android,arch:arm_armv8-a,link:shared
      os:android,image:ramdisk,arch:arm64_armv8-a,link:static
      os:android,image:ramdisk,arch:arm64_armv8-a,link:shared
      os:android,image:vendor_ramdisk,arch:arm64_armv8-a,link:static
      os:android,image:vendor_ramdisk,arch:arm64_armv8-a,link:shared
      os:android,image:recovery,arch:arm64_armv8-a,link:static
      os:android,image:recovery,arch:arm64_armv8-a,link:shared
    
    Change-Id: Id09700450f57abcea903a119f36800ff7fd88090

--- extract-files.py
+++ extract-files.py
@@ -108,6 +108,8 @@ blob_fixups: blob_fixups_user_type = {
         .replace_needed('android.hidl.base@1.0.so', 'libhidlbase.so')
         .add_needed('libbinder_shim.so')
         .add_needed('libinput_shim.so'),
+    ('odm/lib64/libCOppLceTonemapAPI.so', 'odm/lib64/libYTCommon.so', 'odm/lib64/libaps_frame_registration.so'): blob_fixup()
+        .replace_needed('libstdc++.so', 'libstdc++_vendor.so'),
     'vendor/etc/libnfc-nci.conf': blob_fixup()
         .regex_replace('NFC_DEBUG_ENABLED=1', 'NFC_DEBUG_ENABLED=0'),
     'vendor/etc/libnfc-nxp.conf': blob_fixup()
